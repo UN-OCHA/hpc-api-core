bf6116a8103f98005d1f80e86ca530827af11f2a
Pl217 <pl_217@live.com>
2022-04-08 11:38:39 +0200

    ✨ HPC-8349: Introduce `getAllGlobalClustersForPlan`

---
+++ src/lib/data/globalClusters.ts
@@ -0,0 +1,37 @@
+import { Database } from '../../db';
+import { PlanId } from '../../db/models/plan';
+import { Op } from '../../db/util/conditions';
+
+/**
+ * Get all global clusters that are associated with plan's governing entities
+ */
+export const getAllGlobalClustersForPlan = async ({
+  database,
+  planId,
+}: {
+  database: Database;
+  planId: PlanId;
+}) => {
+  const governingEntities = await database.governingEntity.find({
+    where: { planId },
+  });
+
+  const globalClusterAssociations =
+    await database.globalClusterAssociation.find({
+      where: {
+        governingEntityId: {
+          [Op.IN]: governingEntities.map((ge) => ge.id),
+        },
+      },
+    });
+
+  return await database.globalCluster.find({
+    where: {
+      id: {
+        [database.Op.IN]: globalClusterAssociations.map(
+          (gca) => gca.globalClusterId
+        ),
+      },
+    },
+  });
+};
