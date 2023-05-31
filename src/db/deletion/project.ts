147162c4d98f50079d238c9b8c59989ace72eaf2
Pl217 <pl_217@live.com>
2023-05-31 15:46:16 +0200

    HPC-8748: Introduce project deleting util method
    When a project is being deleted, a bunch of
    pre-requisites need to be fulfilled. Then,
    in case that deletion can successfully go
    through, we also need to clean up any references
    to the project, especially those which are
    not cleaned up automatically by DB's
    `ON DELETE CASCADE` foreign key constraints.

---
+++ src/db/deletion/project.ts
@@ -0,0 +1,99 @@
+import Knex = require('knex');
+import type { Database } from '..';
+import { deleteAuthTarget } from '../../auth';
+import { NotFoundError, PreconditionFailedError } from '../../util/error';
+import type { ParticipantId } from '../models/participant';
+import type { ProjectId } from '../models/project';
+
+export const deleteProjectById = async (
+  database: Database,
+  projectToDelete: ProjectId,
+  authGrantRevoker: ParticipantId,
+  trx: Knex.Transaction<any, any>
+) => {
+  const project = await database.project.findOne({
+    where: { id: projectToDelete },
+    trx,
+  });
+
+  if (!project) {
+    throw new NotFoundError(`No project with ID ${projectToDelete}`);
+  }
+
+  // Cannot delete already published project
+  if (project.currentPublishedVersionId) {
+    throw new PreconditionFailedError(
+      `Project with ID ${project.id} has already been published`
+    );
+  }
+
+  if (project.latestVersionId) {
+    const latestVersion = await database.projectVersion.findOne({
+      where: { id: project.latestVersionId },
+      trx,
+    });
+
+    if (latestVersion?.version !== 1) {
+      throw new PreconditionFailedError(
+        `More than one version of project with ID ${project.id} has been created`
+      );
+    }
+  }
+
+  const linkedToFlows = await database.flowObject.find({
+    where: {
+      objectType: 'project',
+      objectID: project.id,
+    },
+    trx,
+  });
+  const totalLinkedFlows = linkedToFlows.length;
+
+  if (totalLinkedFlows) {
+    throw new PreconditionFailedError(
+      `Cannot delete project because it is linked to flows with ID: ${linkedToFlows
+        .map((f) => f.flowID)
+        .slice(0, 10)
+        .join(', ')}.${
+        totalLinkedFlows > 10
+          ? ` There are ${
+              totalLinkedFlows - 10
+            } more flows, we're just showing the first 10`
+          : ''
+      }`
+    );
+  }
+
+  const clonedProjects = await database.project.find({
+    where: { sourceProjectId: project.id },
+    trx,
+  });
+
+  // Unlink all cloned projects
+  await database.project.update({
+    values: { sourceProjectId: null },
+    where: {
+      id: { [database.Op.IN]: clonedProjects.map((p) => p.id) },
+    },
+    trx,
+  });
+
+  await database.project.destroy({
+    where: { id: project.id },
+    trx,
+  });
+
+  // Delete weak references
+  await database.expiredData.destroy({
+    where: { objectType: 'project', objectId: project.id },
+    trx,
+  });
+
+  // Clean up all references in auth tables
+  await deleteAuthTarget(
+    database,
+    { type: 'project', targetId: projectToDelete },
+    authGrantRevoker,
+    trx
+  );
+};
