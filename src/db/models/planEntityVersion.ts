aa05d606cf28e0c73305ad7eb9b10a95472d707a
Pl217 <pl_217@live.com>
2024-12-30 14:42:35 +0100

    HPC-9997: Simplify `support` array in `PLAN_ENTITY_VERSION_VALUE` codec
    After fixing database records so that `support`
    array is initialized everywhere and it can no
    longer have object with key `"0"`, we can simplify
    the codec `PLAN_ENTITY_VERSION_VALUE`

--- src/db/models/planEntityVersion.ts
+++ src/db/models/planEntityVersion.ts
@@ -29,37 +29,22 @@ const PLAN_ENTITY_VERSION_REF = t.partial({
   entityPrototypeId: ENTITY_PROTOTYPE_ID,
 });
 
-export const PLAN_ENTITY_VERSION_VALUE = t.intersection([
-  t.type({
-    categories: t.array(CATEGORY_ID),
-    description: t.string,
-    type: LOCALIZED_PLURAL_STRING,
-  }),
-  t.partial({
-    support: t.union([
-      t.array(
-        t.union([
-          /**
-           * TODO: Some records in the database have `null` inside `support` array,
-           * which is problematic. Stricter validation of data being stored should
-           * be done, existing values removed and then `null` removed from this type.
-           */
-          t.null,
-          PLAN_ENTITY_VERSION_REF,
-        ])
-      ),
+export const PLAN_ENTITY_VERSION_VALUE = t.type({
+  categories: t.array(CATEGORY_ID),
+  description: t.string,
+  type: LOCALIZED_PLURAL_STRING,
+  support: t.array(
+    t.union([
       /**
-       * TODO: Some records have object with key "0" instead of array of objects,
-       * which is absolutely ridiculous. We need to have stricter validation of
-       * data being stored here, convert existing cases to arrays, and then remove
-       * this awful type edge case
+       * TODO: Some records in the database have `null` inside `support` array,
+       * which is problematic. Stricter validation of data being stored should
+       * be done, existing values removed and then `null` removed from this type.
        */
-      t.partial({
-        0: PLAN_ENTITY_VERSION_REF,
-      }),
-    ]),
-  }),
-]);
+      t.null,
+      PLAN_ENTITY_VERSION_REF,
+    ])
+  ),
+});
 export type PlanEntityVersionValue = t.TypeOf<typeof PLAN_ENTITY_VERSION_VALUE>;
 
 export default defineLegacyVersionedModel({
