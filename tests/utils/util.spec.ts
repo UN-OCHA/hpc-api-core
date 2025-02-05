e333b3632acc1519c291d2abb03a4f350ba7f15a
Pl217 <pl_217@live.com>
2025-02-05 15:00:16 +0100

    Add few more unit tests for `toCamelCase`

--- tests/utils/util.spec.ts
+++ tests/utils/util.spec.ts
@@ -2,9 +2,17 @@ import { toCamelCase } from '../../src/util';
 
 describe('Test utility functions', () => {
   it('should convert string to camel case', () => {
+    expect(toCamelCase('')).toBe('');
     expect(toCamelCase('Reached')).toBe('reached');
     expect(toCamelCase('Cumulative reach')).toBe('cumulativeReach');
     expect(toCamelCase('Double  space')).toBe('doubleSpace');
+    expect(toCamelCase('  leading space')).toBe('leadingSpace');
+    expect(toCamelCase('trailing space  ')).toBe('trailingSpace');
+    expect(toCamelCase('  both ends  ')).toBe('bothEnds');
     expect(toCamelCase('with three words')).toBe('withThreeWords');
+    expect(toCamelCase('Mixed CASE')).toBe('mixedCase');
+    expect(toCamelCase('Another Test CASE')).toBe('anotherTestCase');
+    expect(toCamelCase('version 1.0')).toBe('version1.0');
+    expect(toCamelCase('2fast 2furious')).toBe('2fast2furious');
   });
 });
