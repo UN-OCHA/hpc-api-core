af414a4123455cbdeee1a29a05180a84e9844f3e
Onitoxan <anpesalado@gmail.com>
2025-02-19 13:18:28 +0100

    Improve types for externalReference model

--- src/db/models/externalReference.ts
+++ src/db/models/externalReference.ts
@@ -25,7 +25,7 @@ export const EXTERNAL_REFERENCE_IMPORT_INFORMATION = t.partial({
         reason: t.string,
       }),
       t.partial({
-        valueId: t.any,
+        valueId: t.number,
       }),
     ])
   ),
@@ -35,7 +35,7 @@ export const EXTERNAL_REFERENCE_IMPORT_INFORMATION = t.partial({
         key: t.string,
       }),
       t.partial({
-        valueId: t.any,
+        valueId: t.number,
       }),
     ])
   ),
