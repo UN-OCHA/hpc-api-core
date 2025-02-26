91bcd64bb0834d113ca2aa925941d86650329ee6
Pl217 <pl_217@live.com>
2025-02-27 00:12:52 +0100

    [TEMP] Add `UnprocessableEntityError` error class

--- src/util/error.ts
+++ src/util/error.ts
@@ -188,3 +188,29 @@ export class PreconditionFailedError extends ExtendableError {
     this.body = { status: 'error', code: this.name, message };
   }
 }
+
+export class UnprocessableEntityError extends ExtendableError {
+  private readonly _originalError?: Error;
+  private readonly body: ErrorBody;
+  private readonly statusCode: StatusCode;
+
+  constructor(message: string, originalError?: Error) {
+    super(message);
+
+    /*
+     * Due to a known issue (https://github.com/microsoft/TypeScript/issues/13965)
+     * we need to set prototype manually, in order to get `instanceof` to work
+     * for classes that extend `Error`
+     */
+    Object.setPrototypeOf(this, UnprocessableEntityError.prototype);
+
+    this.name = this.constructor.name;
+    this.statusCode = STATUS_CODES.UNPROCESSABLE_ENTITY;
+
+    if (originalError) {
+      this._originalError = originalError;
+    }
+
+    this.body = { status: 'error', code: this.name, message };
+  }
+}
