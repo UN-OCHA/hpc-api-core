UN-OCHA/hpc-api-core

commit 5969d2f58f83c5c0ad8f4f981e688e1b0d6ecb64

Check existing email for first HID login too
The existing code was written based on assumption
that users switching to Entra ID already used HID
before, so if the same email was used for HID that
is now used for Entra ID, the same record in
`participant` table would be kept.

But, that assumption is not necessarily true.
While most users would have used HID before switching
to Entra ID, it doesn't have to mean that they used
same email address for both.

The fix wants to improve the situation if users
first log into our system with Entra ID, then create
HID with that same email address, regardless if they
already have HID accounts with other email addresses.

src/auth/index.ts
@@ -145,19 +145,18 @@ export const getLoggedInParticipant = async (
   });
 
   if (!participant) {
-    if (isEntraIDToken) {
-      // Check if there's a participant with this email
-      // address already, presumably created via HID
-      participant = await models.participant.findOne({
-        where: { email },
-      });
+    // Check if there's a participant with this email address already
+    participant = await models.participant.findOne({
+      where: { email },
+    });
 
-      if (participant) {
-        await models.participant.update({
-          values: { entraId: sub },
-          where: { id: participant.id },
-        });
-      }
+    if (participant) {
+      const otherSubProperty = isEntraIDToken ? 'hidSub' : 'entraId';
+
+      await models.participant.update({
+        values: { [otherSubProperty]: sub },
+        where: { id: participant.id },
+      });
     }
 
     // Create a new participant for this HID/Entra ID account

commit 57a5bac0718ce346cec454760b1ff01cb33d2d6b

🐛 Pick correct sub property
This fixes a bug introduced in 5969d2f58f83c5c0ad8f4f981e688e1b0d6ecb64.

When user logs in for the first time with Entra ID,
while previously have used HID, we should find user
with that email and update `entraId` property.

Also, when they log in for the first time with HID,
while previously have used Entra ID, we should
update `hidSub` property.

src/auth/index.ts
@@ -151,10 +151,8 @@ export const getLoggedInParticipant = async (
     });
 
     if (participant) {
-      const otherSubProperty = isEntraIDToken ? 'hidSub' : 'entraId';
-
       await models.participant.update({
-        values: { [otherSubProperty]: sub },
+        values: { [subProperty]: sub },
         where: { id: participant.id },
       });
     }